a4470ecccac3aa8070c674a66ee8bf5d5eade527
5730196321-DK <jrkns1996@gmail.com>
2017-04-17 00:19:43 +0700

    Finish Student Things Up

--- routes/index_std.js
+++ routes/index_std.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index_std', { title: 'Index' });
+  res.render('index_std', { title: 'Home' });
 });
 
 module.exports = router;
--- routes/profile_std.js
+++ routes/profile_std.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('account_std', { title: 'Account' });
+  res.render('profile_std', { title: 'Profile Manager' });
 });
 
 module.exports = router;
