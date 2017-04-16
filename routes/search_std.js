7325cd6b32d3a828ef311d54dee93d537e1a742f
5730196321-DK <jrkns1996@gmail.com>
2017-04-16 17:30:09 +0700

    Add Activity, Internship

--- routes/search_std.js
+++ routes/search_std.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('search_std', { title: 'Search Courses Info' });
+  res.render('search_std', { title: 'Search Courses' });
 });
 
 module.exports = router;
